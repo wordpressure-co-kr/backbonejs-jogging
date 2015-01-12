fdc47b4e5588eb733b823ed0cd04158c62f72b7e
Kwag De Gi <tech@skyaperture.com>
2015-01-12 10:57:27 +0900

    add button added

--- 0112/0112.js
+++ 0112/0112.js
@@ -1,14 +1,27 @@
 (function($){
 	var ListView = Backbone.View.extend({
 		el: $('body'),
-		initialize: function() {
-			_.bindAll(this, 'render');
 
-			this.render();
+		events: {
+			'click button#add': 'addItem'
 		},
-	
+
+		initialize: function() {
+			_.bindAll(this, 'render', 'addItem');
+
+			this.counter = 0;
+			this.render();
+
+		},
+
 		render: function() {
-			$(this.el).append("<ul> <li> hi world </li> </ul>");
+			$(this.el).append("<button id='add'>Add list item</button>");
+			$(this.el).append("<ul></ul>");
+		},
+
+		addItem: function() {
+			this.counter++;
+			$('ul', this.el).append("<li>hi world "+ this.counter + "</li>");
 		}
 	});
 
