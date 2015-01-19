46a1192239908420808fc74c0cf5924b046f65cc
Kwag De Gi <tech@skyaperture.com>
2015-01-19 18:55:23 +0900

    model view action added

--- 0119/app.js
+++ 0119/app.js
@@ -1,8 +1,13 @@
 (function($){
+
+	Backbone.sync = function(method, model, success, error){
+		success();
+	}
+
 	var Item = Backbone.Model.extend({
 		defaults: {
 			part1: 'Hi',
-	    		part2: 'World'
+			part2: 'World'
 		}
 	});
 
@@ -12,22 +17,49 @@
 
 	var ItemView = Backbone.View.extend({
 		tagName: 'li',
-	    	initialize: function(){
-			_.bindAll(this, 'render');
+	    
+	    events: {
+	    	'click span.swap': 'swap',
+	    	'click span.delete': 'remove'
+	    },	
+
+		initialize: function(){
+			_.bindAll(this, 'render', 'unrender', 'swap', 'remove');
+			this.model.bind('change', this.render);
+			this.model.bind('remove', this.unrender);
+
 		},
-	    	render: function(){
-			$(this.el).html('<span>'+this.model.get('part1')+' '+this.model.get('part2')+'</span>');
+
+		render: function(){
+			$(this.el).html('<span style="color:black;">'+this.model.get('part1')+' '+this.model.get('part2')+'</span> &nbsp; &nbsp; <span class="swap" style="font-family:sans-serif; color:blue; cursor:pointer;">[swap]</span> <span class="delete" style="cursor:pointer; color:red; font-family:sans-serif;">[delete]</span>');
 			return this;
+		},
+
+		unrender: function(){
+			$(this.el).remove();
+		},
+
+		swap: function(){
+			var swapped = {
+				part1: this.model.get('part2'),
+				part2: this.model.get('part1')
+			};
+			this.model.set(swapped);
+		},
+
+		remove: function(){
+			this.model.destroy();
 		}
+
 	});
 
 	var ListView = Backbone.View.extend({
 		el: $('body'),
-	    	events: {
+	    events: {
 			'click button#add': 'addItem'
 		},
 
-	    	initialize: function(){
+	    initialize: function(){
 			_.bindAll(this, 'render', 'addItem', 'appendItem');
 
 			this.collection = new List();
@@ -37,7 +69,7 @@
 			this.render();
 		},
 
-	    	render: function(){
+	    render: function(){
 			var self = this;
 			$(this.el).append("<button id='add'>Add list item </button>");
 			$(this.el).append("<ul></ul>");
